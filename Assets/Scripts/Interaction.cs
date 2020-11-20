19fa9bd9f4710570fa58f7eb8b1fdffe6bae5af1
ThibCL <thibault.cabourg@gmai.com>
2020-11-21 00:41:13 +0100

    click

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Interaction : MonoBehaviour {
+    public float clickDistance = 5;
+
     private void OnMouseDown() {
-        Debug.Log("Click");
+        var player = GameObject.Find("Player");
+        var dist = Vector3.Distance(transform.position, player.transform.position);
+        if (dist < clickDistance) {
+            Debug.Log("Click");
+        }
     }
 }

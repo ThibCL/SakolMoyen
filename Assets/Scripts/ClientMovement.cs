681ef2bb916d8e796e663793ec6534451436c215
ThibCL <thibault.cabourg@gmai.com>
2020-11-21 12:18:46 +0100

    path finding

---
+++ Assets/Scripts/ClientMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClientMovement : MonoBehaviour {
+    public Transform player;
+    public NavMeshAgent agent;
+    public float waitDistance;
+
+    public bool follow = false;
+
+    void Update() {
+        if (follow) {
+            var waitPos = Vector3.ClampMagnitude(player.position - transform.position, waitDistance);
+            agent.SetDestination(player.position - waitPos);
+        }
+    }
+
+    private void OnMouseDown() {
+        Debug.Log("Click");
+        follow = !follow;
+    }
+}
